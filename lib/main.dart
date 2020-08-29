a26b81abfd7f1759192467d07007778b1ddbd50a
wadthyk <66349360+wadthyk@users.noreply.github.com>
2020-08-29 16:27:38 +0900

    Change-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+//The main function is the starting point for all of our Flutter apps
 void main() {
+<<<<<<< HEAD
   runApp(MyApp());
 }
 
@@ -96,13 +98,38 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
+=======
+  var appBar2 = AppBar(
+    title: Text('First App from Wadthy'),
+    backgroundColor: Colors.blueGrey[700],
+  );
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: appBar2,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          tooltip: 'Increment',
+          child: Icon(Icons.traffic),
+          elevation: 2.0,
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 60.0,
+            color: Colors.blueAccent[100],
+          ),
+          shape: CircularNotchedRectangle(),
+          color: Colors.blueGrey,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/Monty_1.png'),
+          ),
+>>>>>>> Change-3
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ),
+  );
 }
