d04a25fd19273ba03dcb3dcfbbae08c0c8eb0beb
wadthyk <66349360+wadthyk@users.noreply.github.com>
2020-09-05 17:50:36 +0900

    functional_buttombar_green

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,104 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 //The main function is the starting point for all of our Flutter apps
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey[100],
-        appBar: AppBar(title: Text('Monty App'), backgroundColor: Colors.green),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.green,
-          child: Container(
-            height: 50.0,
-          ),
-        ),
-        body: Center(
-          child: Image(
-            height: 300,
-            width: 300,
-            image: AssetImage('images/Monty_1.png'),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  HomePage createState() => HomePage();
+}
+
+class HomePage extends State<MainPage> {
+  //PageController _myPage = PageController(initialPage: 0);
+  int currentIndex;
+
+  @override
+  //_MyApp createState() => MyApp();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Container(
+        height: 65.0,
+        width: 65.0,
+        child: FittedBox(
+          child: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+            backgroundColor: Colors.green,
           ),
         ),
       ),
-    ),
-  );
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: Colors.green,
+        child: Container(
+          height: 65.0,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                iconSize: 30.0,
+                padding: EdgeInsets.only(left: 28.0),
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  setState(() {
+                    currentIndex = 0;
+                    //change tab (Home.dart)
+                    print("${currentIndex}");
+                  });
+                },
+              ),
+              IconButton(
+                iconSize: 30.0,
+                padding: EdgeInsets.only(right: 28.0),
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  setState(() {
+                    currentIndex = 1;
+                    print("${currentIndex}");
+                  });
+                },
+              ),
+              IconButton(
+                iconSize: 30.0,
+                padding: EdgeInsets.only(left: 28.0),
+                icon: Icon(Icons.notifications),
+                onPressed: () {
+                  setState(() {
+                    currentIndex = 2;
+                    print("${currentIndex}");
+                  });
+                },
+              ),
+              IconButton(
+                  iconSize: 30.0,
+                  padding: EdgeInsets.only(right: 28.0),
+                  icon: Icon(Icons.list),
+                  onPressed: () {
+                    setState(() {
+                      currentIndex = 3;
+                      print("${currentIndex}");
+                    });
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
